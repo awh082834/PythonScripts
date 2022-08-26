1185a8907caa0d11d165408380ce091955c0b7c5
andrew.hale@dgs.virginia.gov <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>
2022-08-26 14:53:50 +0000

    Added msg outputs for IsolateContigs.py

--- IsolateContigs.py
+++ IsolateContigs.py
@@ -6,23 +6,39 @@ import os.path
 assemFile = sys.argv[1]
 #Name of output file
 outfileName = "IsolatedContigs.fasta"
-headerFound = False
-secondHeaderFound = False
+notFirstContig = False
 try:
     with open(assemFile, 'r') as infile:
         lines = infile.readlines()
-        #If the user wants all contigs other than the first, presumably
-        #chromosomal contig, this flag will grab all of them and 
-        #add them to a new file.
-        for line in lines:
-            if secondHeaderFound:
-                outfile.write(line)
-            #Looks for the second contig as that is the second
-            #smallest, excluding the largest, the chromosomal contig
-            if ">2" in line:
-                secondHeaderFound = True
-                outfile = open(outfileName, 'a')
-                outfile.write(line)
 
+        #Tracks overall contig number
+        ContigNum = 0
+
+        #Tracks circular contig number
+        circContigNum = 0
+        for line in lines:
+
+            #Checks for header identifier
+            if '>' in line:
+                ContigNum += 1
+
+                #If the contig is circular and not the first contig 
+                #(This assumes that the first circular contig is the chromosomal sequence, we are looking for MGEs or plasmids.)
+                if 'circular=true' in line and  ContigNum != 1:
+
+                    #Will only count circular contigs other than circular chromosomal sequence
+                    circContigNum += 1
+
+                    #Flags that program is past the first contig
+                    notFirstContig = True
+                    
+                    #Begins writing to output fasta
+                    outfile = open(outfileName,'a')
+                    outfile.write(line)
+                    
+            elif notFirstContig == True:
+                outfile.write(line)
+        outfile.close()
+        print(str(circContigNum) + " contig(s) have been found to be circularized and were isolated from main chromosomal sequence or rest of assembly. \nThese were output to IsolatedContigs.fasta")
 except:
     print("Assembly file does not exist.")
